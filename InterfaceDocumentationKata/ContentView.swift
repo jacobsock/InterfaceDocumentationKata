e61d9b9684f09fe8de56a2d1eac067859431730a
Zoe Cutler <zcutlerf@gmail.com>
2023-02-15 14:02:31 -0500

    Initial Commit

---
+++ InterfaceDocumentationKata/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  InterfaceDocumentationKata
+//
+//  Created by Zoe Cutler on 2/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InterfaceDocumentationKata/InterfaceDocumentationKataApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceDocumentationKataApp.swift
+//  InterfaceDocumentationKata
+//
+//  Created by Zoe Cutler on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceDocumentationKataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
